d60340e57bb09194046b867f203d856a512107ab
chandra694 <K C K@LAPTOP-NS62KLMK>
2021-11-30 18:17:39 +0530

    my first commit

---
+++ src/Demo.java
@@ -0,0 +1,15 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		String hunger = "yes";
+
+		if (hunger == "yes") {
+			System.out.println("eat something");
+		} else {
+			System.out.println("have a good sleep");
+		}
+	}
+
+}
